a0323e7d866caf6b3c1134bef65e5ed139b8f38d
Jacob Drath <89097881+jacobd082@users.noreply.github.com>
2022-08-20 23:58:34 +0000

    Fix

--- print.php
+++ print.php
@@ -3,9 +3,8 @@
 print $_GET['txt'];
 
 
-$myfile = fopen("log.txt", "w") or die("Unable to open file!");
-$fc = fread($myfile,filesize("log.txt"));
-$txt = $fc . $_GET['txt'] . "\n";
+$myfile = fopen("log.txt", "a") or die("Unable to open file!");
+$txt = $_GET['txt'] . "\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 
