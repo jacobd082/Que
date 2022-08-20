20aedb68c32b4347050c1e398a571ec1d25303d3
Jacob Drath <89097881+jacobd082@users.noreply.github.com>
2022-08-20 17:03:04 +0000

    Fix

--- submit-report.php
+++ submit-report.php
@@ -1,6 +1,33 @@
 <html>
   <body>
     <?php
+
+//The url you wish to send the POST request to
+$url = 'https://kv.replit.com/v0/eyJhbGciOiJIUzUxMiIsImlzcyI6ImNvbm1hbiIsImtpZCI6InByb2Q6MSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb25tYW4iLCJleHAiOjE2NjExMTI0OTAsImlhdCI6MTY2MTAwMDg5MCwiZGF0YWJhc2VfaWQiOiI0YzY1ZmUxYS03YzRkLTQyNDktYmJkNC0wYjJkZTE1NjVjODgiLCJ1c2VyIjoiSmFjb2JEZXZlbG9wZXIiLCJzbHVnIjoiUXVlIn0.A7OL2KEV5s5VArDaKQWVk2UYij1h9uxBbseoJTUWPbk2kJawGQbe8isULzG1Bfo-B7DkcbMM5w0xRwjtn-qQWA';
+
+//The data you want to send via POST
+$fields = [
+    'q'      => $_POST['txt']
+];
+
+//url-ify the data for the POST
+$fields_string = http_build_query($fields);
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, true);
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+//So that curl_exec returns the contents of the cURL; rather than echoing it
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+//execute post
+$result = curl_exec($ch);
+echo $result;
+/*
       // create curl resource 
         $ch = curl_init(); 
 
@@ -15,6 +42,7 @@
 
         // close curl resource to free up system resources 
         curl_close($ch);  
+    */
     ?>
   </body>
 </html>
