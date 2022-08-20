65c81cd29f76dba1c371f2b3eed9d0ea9088ca6d
Jacob <89097881+jacobd082@users.noreply.github.com>
2022-08-20 10:11:23 -0500

    Added testing capactcha

---
+++ human/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>reCAPTCHA</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  </head>
+  <body>
+    <h1>Please Verify</h1>
+    <form action="verify.php" method="POST">
+      <div class="g-recaptcha" data-sitekey="6LfB4ckfAAAAAJH3qOotGiFW1Munvvy_o9hC_8AU"></div>
+      <br/>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
---
+++ human/verify.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>reCAPTCHA</title>
+</head>
+<body>
+<?php
+//The url you wish to send the POST request to
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+
+//The data you want to send via POST
+$fields = [
+    'response'      => $_POST['response'],
+    'secret' => '6LfB4ckfAAAAANmItrzwta9-6SODkowTsoqGpPSo'
+];
+
+//url-ify the data for the POST
+$fields_string = http_build_query($fields);
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, true);
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+//So that curl_exec returns the contents of the cURL; rather than echoing it
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+//execute post
+$result = curl_exec($ch);
+echo $result;
+?>
+</body>
+</html>
