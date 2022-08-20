328154657fabf99b4763d8e105cf59869f2dec0b
Jacob Drath <89097881+jacobd082@users.noreply.github.com>
2022-08-20 16:59:17 +0000

    Fix

--- submit-report.php
+++ submit-report.php
@@ -5,7 +5,7 @@
         $ch = curl_init(); 
 
         // set url 
-        curl_setopt($ch, CURLOPT_URL, "https://kv.replit.com/v0/eyJhbGciOiJIUzUxMiIsImlzcyI6ImNvbm1hbiIsImtpZCI6InByb2Q6MSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb25tYW4iLCJleHAiOjE2NjExMTI0OTAsImlhdCI6MTY2MTAwMDg5MCwiZGF0YWJhc2VfaWQiOiI0YzY1ZmUxYS03YzRkLTQyNDktYmJkNC0wYjJkZTE1NjVjODgiLCJ1c2VyIjoiSmFjb2JEZXZlbG9wZXIiLCJzbHVnIjoiUXVlIn0.A7OL2KEV5s5VArDaKQWVk2UYij1h9uxBbseoJTUWPbk2kJawGQbe8isULzG1Bfo-B7DkcbMM5w0xRwjtn-qQWA?q={$_GET['txt']}"); 
+        curl_setopt($ch, CURLOPT_URL, "https://kv.replit.com/v0/eyJhbGciOiJIUzUxMiIsImlzcyI6ImNvbm1hbiIsImtpZCI6InByb2Q6MSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb25tYW4iLCJleHAiOjE2NjExMTI0OTAsImlhdCI6MTY2MTAwMDg5MCwiZGF0YWJhc2VfaWQiOiI0YzY1ZmUxYS03YzRkLTQyNDktYmJkNC0wYjJkZTE1NjVjODgiLCJ1c2VyIjoiSmFjb2JEZXZlbG9wZXIiLCJzbHVnIjoiUXVlIn0.A7OL2KEV5s5VArDaKQWVk2UYij1h9uxBbseoJTUWPbk2kJawGQbe8isULzG1Bfo-B7DkcbMM5w0xRwjtn-qQWA?q={$_POST['txt']}"); 
 
         //return the transfer as a string 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
