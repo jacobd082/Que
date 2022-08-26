bc95fcfabfcf5cf806f5bc68e20cc597f8440e50
Jacob Drath <89097881+jacobd082@users.noreply.github.com>
2022-08-26 12:12:27 -0500

    Update index.php

--- human/index.php
+++ human/index.php
@@ -45,7 +45,27 @@ function done() {
       </script>
       
       <a href="https://stats.uptimerobot.com/XXPxmf4P2w" style="color: gray;">Status</a>&nbsp;<a href="https://que.jacobdrath.co/pages/security.html" style="color: gray;">Security</a>&nbsp;<a href="https://que.jacobdrath.co/pages/providers.html" style="color: gray;">Our providers</a>
+    &nbsp;<a href="https://que.jacobdrath.co/pages/policy.html" style="color: gray;">Privacy Policy</a>
     </center>
+  </body>
+  <style>
+        body {
+        background-color: rgb(17, 0, 31);
+        color: black;
+        font-family: 'Open Sans', sans-serif;
+        }
+        #sub_but {
+          font-size: 14px;
+          cursor: pointer;
+          border: none;
+          padding: 8px;
+          border-radius: 10px;
+          background: rgb(178,82,245);
+          background: linear-gradient(90deg, rgba(178,82,245,1) 0%, rgba(112,62,222,1) 35%, rgba(0,212,255,1) 100%);
+          }
+  </style>
+</html>
+</center>
   </body>
   <style>
         body {
