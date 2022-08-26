509b81cdb69130e75f57a984f1af73bf1020a429
Jacob <89097881+jacobd082@users.noreply.github.com>
2022-08-26 10:58:44 -0500

    Added counter

--- print.php
+++ print.php
@@ -1,11 +1,21 @@
 <?php
 
 print $_GET['txt'];
+$myfile = fopen("log.txt", "w+") or die("Unable to open file!");
+
+$txt = fread($myfile,filesize("log.txt"));
+$numtxt = (int)$txt;
+$newnum = $numtxt + 1;
+echo $newnum;
+fwrite($myfile, $newnum);
+fclose($myfile);
+/*
+print $_GET['txt'];
 
 
 $myfile = fopen("log.txt", "a") or die("Unable to open file!");
 $txt = $_GET['txt'] . ",";
 fwrite($myfile, $txt);
 fclose($myfile);
-
+*/
 ?>
